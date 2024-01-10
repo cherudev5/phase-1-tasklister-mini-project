afa87583e27280c35036765f4f2da9883b541435
David <cheruiyotscripts@gmail.com>
2024-01-10 08:50:15 +0300

    completed asiignmnet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks');
+  
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const taskInput = document.querySelector('#new-task-description');
+    const task = taskInput.value;
+    if (task === '') {
+      alert('Please enter a task!');
+      return;
+    }
+    const taskItem = document.createElement('li');
+    taskItem.textContent = task;
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
+  });
+  
 });
